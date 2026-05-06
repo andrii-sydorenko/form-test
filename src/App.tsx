cf271a5a42813b6bc7918afd94e4e5bef95c7a55
Andrii <rey.nko12@gmail.com>
2026-05-06 15:05:46 +0300

    Init app

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { useState, type ChangeEvent } from "react";
+import "./styles.css";
+
+const COMPANIES = [
+  { name: "dev-vortexrestaurantequipment.pantheon.io", techSpend: 0 },
+  { name: "goaccess.kpmg.com", techSpend: 0 },
+  { name: "qa-dashboard-edge.ihg.com", techSpend: 0 },
+  { name: "oecexcellence.gehealthcare.com", techSpend: 0 },
+  { name: "ads.scotiabank.com/*", techSpend: 0 },
+  { name: "onboardinghub-stage.mastercard.com", techSpend: 0 },
+  { name: "agilent.com", techSpend: 0 },
+];
+
+const RowItem = ({ name, techSpend }: { name: string, techSpend: number }) => {
+  const [val, setVal] = useState(techSpend);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+
+    setVal(Number(value));
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        marginBottom: 10,
+      }}
+    >
+      <span>{name}</span>
+      <input value={val} onChange={handleChange} />
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <div className="App">
+      {COMPANIES.map(({ name, techSpend }) => {
+        return <RowItem name={name} techSpend={techSpend} />;
+      })}
+    </div>
+  );
+}
